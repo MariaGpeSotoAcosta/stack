fe953f07519059c490ebf505806f25d7f7676969
MariaGpeSotoAcosta <A00228158@tec.mx>
2024-10-15 20:43:06 -0600

    Update Stack.cpp

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -1,1 +1,40 @@
 #include "Stack.h"
+#include <iostream>
+using namespace std;
+
+Stack::Stack() : topNode(nullptr) {}
+
+Stack::~Stack() {
+    while (!isEmpty()) {
+        pop();
+    }
+}
+
+void Stack::push(int value) {
+    Node* newNode = new Node();
+    newNode->data = value;
+    newNode->next = topNode;
+    topNode = newNode;
+}
+
+void Stack::pop() {
+    if (isEmpty()) {
+        cout << "El stack está vacío" << endl;
+        return;
+    }
+    Node* temp = topNode;
+    topNode = topNode->next;
+    delete temp;
+}
+
+int Stack::top() const {
+    if (isEmpty()) {
+        cout << "El stack está vacío" << endl;
+        return -1;
+    }
+    return topNode->data;
+}
+
+bool Stack::isEmpty() const {
+    return topNode == nullptr;
+}
